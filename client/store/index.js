532adcdfc31ef0469cd17dafc85970b8fd63d63c
Kate Norton <katernorton@outlook.com>
2020-07-10 11:06:51 -0400

    fixed fogetting to import currentView

--- client/store/index.js
+++ client/store/index.js
@@ -4,10 +4,12 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import graphOption from './graphOption'
+import currentView from './currentView'
 
 const reducer = combineReducers({
   user,
   graphOption,
+  currentView,
 })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
