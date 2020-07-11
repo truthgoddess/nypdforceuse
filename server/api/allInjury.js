ac33b06601da58a0cc882e85b8b9375398729824
Kate Norton <katernorton@outlook.com>
2020-07-11 14:16:01 -0400

    placeholder get routes made

--- server/api/allInjury.js
+++ server/api/allInjury.js
@@ -17,28 +17,29 @@ module.exports = router
 //1  a      a      a       a
 //2  a      a      a       :
 //3  a      a      :       a
-//4  a      :      a       a
-//5  :      a      a       a
 //6  a      a      :       :
+//4  a      :      a       a
 //7  a      :      a       :
 //8  a      :      :       a
-//9  :      a      :       a
-//10 :      a      a       :
-//11 :      :      a       a
 //12 a      :      :       :
+//5  :      a      a       a
+//10 :      a      a       :
+//9  :      a      :       a
 //13 :      a      :       :
+//11 :      :      a       a
 //14 :      :      a       :
 //15 :      :      :       a
 //16 :      :      :       :
 
-router.get('/', async function (req, res, next) {
-  const year = req.query.year ? req.query.year : false
-  const quarter = req.query.quarter ? req.query.quarter : false
-  const duty = req.query.duty ? req.query.duty : false
-  const command = req.query.duty ? req.query.duty : false
+// leaving because may have to use the idea below
+// router.get('/', async function (req, res, next) {
+//   const year = req.query.year ? req.query.year : false
+//   const quarter = req.query.quarter ? req.query.quarter : false
+//   const duty = req.query.duty ? req.query.duty : false
+//   const command = req.query.duty ? req.query.duty : false
 
-  const officerInjuryData = await OfficerInjury.findAll({})
-})
+//   const officerInjuryData = await OfficerInjury.findAll({})
+// })
 
 //1 a      a      a       a  <--All Time, All Injuries, Off + On Duty, All Commands
 router.get('/allYear/allQuarter/allDuty/allCommand', async function (
@@ -70,21 +71,6 @@ router.get('allYear/AllQuarter/allDuty/:command', async function (
   }
 })
 
-//6  a      a      :       :  <--AllTime, All Injuries, Specific Duty, Specific Command
-router.get('/allYear/allQuarter/:duty/:command', async function (
-  req,
-  res,
-  next
-) {
-  try {
-    console.log(
-      'api/graphData/allInjury/allYear/allQuarter/:duty/:command route'
-    )
-  } catch (error) {
-    next(error)
-  }
-})
-
 //3  a      a      :       a  <--All Time, All Injuries, Specific Duty, All Command
 router.get('/allYear/allQuarter/:duty/allCommand', async function (
   req,
@@ -94,6 +80,21 @@ router.get('/allYear/allQuarter/:duty/allCommand', async function (
   try {
     console.log(
       'api/graphData/allInjury/allYear/allQuarter/:duty/allCommand route'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//6  a      a      :       :  <--AllTime, All Injuries, Specific Duty, Specific Command
+router.get('/allYear/allQuarter/:duty/:command', async function (
+  req,
+  res,
+  next
+) {
+  try {
+    console.log(
+      'api/graphData/allInjury/allYear/allQuarter/:duty/:command route'
     )
   } catch (error) {
     next(error)
@@ -115,6 +116,45 @@ router.get('/allYear/:quarter/allDuty/allCommand/', async function (
   }
 })
 
+//7  a      :      a       :  <--All Years, Specific Quarter, All Injuries, All Duty, Specific Command
+router.get('/allYear/:quarter/allDuty/:command', async function (
+  req,
+  res,
+  next
+) {
+  try {
+    console.log(
+      'api/graphData/allInjury/allYear/:quarter/allDuty/:command route'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//8  a      :      :       a  <--All Years, Specific Quarter, All Injuries, Specific Duty, All Command
+router.get('allYear/:quarter/:duty/allCommand', async function (
+  req,
+  res,
+  next
+) {
+  try {
+    console.log(
+      'api/graphData/allInjury/allYear/:quarter/:duty/allCommand route'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//12 a      :      :       :  <--All Years, Specific Quarters, All Injuries, Specific Duty, Specific Command
+router.get('/allYear/:quarter/:duty/:command', async function (req, res, next) {
+  try {
+    console.log('api/graphData/allInjury/allYear/:quarter/:duty/:command')
+  } catch (error) {
+    next(error)
+  }
+})
+
 //5  :      a      a       a  <--Specific Year, All Quarters, All Injuries, All Duty, All Command
 router.get('/:year/allQuarter/allDuty/allCommand', async function (
   req,
@@ -130,46 +170,6 @@ router.get('/:year/allQuarter/allDuty/allCommand', async function (
   }
 })
 
-//7  a      :      a       :  <--All Years, Specific Quarter, All Injuries, All Duty, Specific Command
-router.get('/allYear/:quarter/allDuty/:command', async function (
-  req,
-  res,
-  next
-) {
-  try {
-    console.log(
-      'api/graphData/allInjury/allYear/:quarter/allDuty/:command route'
-    )
-  } catch (error) {
-    next(error)
-  }
-})
-//8  a      :      :       a  <--All Years, Specific Quarter, All Injuries, Specific Duty, All Command
-router.get('allYear/:quarter/:duty/allCommand', async function (
-  req,
-  res,
-  next
-) {
-  try {
-    console.log(
-      'api/graphData/allInjury/allYear/:quarter/:duty/allCommand route'
-    )
-  } catch (error) {
-    next(error)
-  }
-})
-//9  :      a      :       a  <--Specific Year, All Quarters, All Injuries, Specific Duty, All Command
-router.get('/:year/allQuarter/:duty/allCommand', async function (
-  req,
-  res,
-  next
-) {
-  try {
-    console.log('api/graphData/allInjury/:year/allQuarter/:duty/allCommand')
-  } catch (error) {
-    next(error)
-  }
-})
 //10 :      a      a       :  <--Specific Year, All Quarters, All Injuries, All Duty, Specific Command
 router.get('/:year/allQuarter/allDuty/:command', async function (
   req,
@@ -182,6 +182,29 @@ router.get('/:year/allQuarter/allDuty/:command', async function (
     next(error)
   }
 })
+
+//9  :      a      :       a  <--Specific Year, All Quarters, All Injuries, Specific Duty, All Command
+router.get('/:year/allQuarter/:duty/allCommand', async function (
+  req,
+  res,
+  next
+) {
+  try {
+    console.log('api/graphData/allInjury/:year/allQuarter/:duty/allCommand')
+  } catch (error) {
+    next(error)
+  }
+})
+
+//13 :      a      :       :  <--Specific Year, All Quarters, All Injuries, Specific Duty, Specific Command
+router.get('/:year/allQuarter/:duty/:command', async function (req, res, next) {
+  try {
+    console.log('api/graphData/allInjury/:year/allQuarter/:duty/:command')
+  } catch (error) {
+    next(error)
+  }
+})
+
 //11 :      :      a       a  <--Specific Year, Specific Quarter, All Injuries, All Duty, All Command
 router.get('/:year/:quarter/allDuty/allCommand', async function (
   req,
@@ -194,22 +217,7 @@ router.get('/:year/:quarter/allDuty/allCommand', async function (
     next(error)
   }
 })
-//12 a      :      :       :  <--All Years, Specific Quarters, All Injuries, Specific Duty, Specific Command
-router.get('/allYear/:quarter/:duty/:command', async function (req, res, next) {
-  try {
-    console.log('api/graphData/allInjury/allYear/:quarter/:duty/:command')
-  } catch (error) {
-    next(error)
-  }
-})
-//13 :      a      :       :  <--Specific Year, All Quarters, All Injuries, Specific Duty, Specific Command
-router.get('/:year/allQuarter/:duty/:command', async function (req, res, next) {
-  try {
-    console.log('api/graphData/allInjury/:year/allQuarter/:duty/:command')
-  } catch (error) {
-    next(error)
-  }
-})
+
 //14 :      :      a       :  <--Specific Year, Specific Quarters, All Injuries, All Duty, Specific Command
 router.get('/:year/:quarter/allDuty/:command', async function (req, res, next) {
   try {
@@ -218,6 +226,7 @@ router.get('/:year/:quarter/allDuty/:command', async function (req, res, next) {
     next(error)
   }
 })
+
 //15 :      :      :       a  <--Specific Year, Specific Quarters, All Injuries, Specific Duty, All Command
 router.get('/:year/:quarter/:duty/allCommand', async function (req, res, next) {
   try {
@@ -226,6 +235,7 @@ router.get('/:year/:quarter/:duty/allCommand', async function (req, res, next) {
     next(error)
   }
 })
+
 //16 :      :      :       :  <--Specific Year, Specific Quarters, All Injuries, Specific Duty, Specific Command
 router.get('/:year/:quarter/:duty/:command', async function (req, res, next) {
   try {
--- server/api/incidentsBasisEncounter.js
+++ server/api/incidentsBasisEncounter.js
@@ -13,13 +13,239 @@ const {
 } = require('../db/models')
 module.exports = router
 
-router.get('/:year/:quarter/:onOffDuty/:command', async function (
+//year, quarter, duty, command
+//1  a      a      a       a
+//2  a      a      a       :
+//3  a      a      :       a
+//6  a      a      :       :
+//4  a      :      a       a
+//7  a      :      a       :
+//8  a      :      :       a
+//12 a      :      :       :
+//5  :      a      a       a
+//10 :      a      a       :
+//9  :      a      :       a
+//13 :      a      :       :
+//11 :      :      a       a
+//14 :      :      a       :
+//15 :      :      :       a
+//16 :      :      :       :
+
+//1 a      a      a       a  <--All Time, All Injuries, Off + On Duty, All Commands
+router.get('/allYear/allQuarter/allDuty/allCommand', async function (
   req,
   res,
   next
 ) {
   try {
-    console.log('Officer Injury')
+    console.log(
+      'api/graphData/incidentsBasisEncounter/allYear/allQuarter/allDuty/allCommand route'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//2  a      a      a       :  <--All Time, All Injuries, Off + On Duty, Specific Command
+router.get('allYear/AllQuarter/allDuty/:command', async function (
+  req,
+  res,
+  next
+) {
+  try {
+    console.log(
+      'api/graphData/incidentsBasisEncounter/allYear/AllQuarter/allDuty/:command route'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//3  a      a      :       a  <--All Time, All Injuries, Specific Duty, All Command
+router.get('/allYear/allQuarter/:duty/allCommand', async function (
+  req,
+  res,
+  next
+) {
+  try {
+    console.log(
+      'api/graphData/incidentsBasisEncounter/allYear/allQuarter/:duty/allCommand route'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//6  a      a      :       :  <--AllTime, All Injuries, Specific Duty, Specific Command
+router.get('/allYear/allQuarter/:duty/:command', async function (
+  req,
+  res,
+  next
+) {
+  try {
+    console.log(
+      'api/graphData/incidentsBasisEncounter/allYear/allQuarter/:duty/:command route'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//4  a      :      a       a  <--All Years, Specific Quarter, All Injuries, All Duty, All Command
+router.get('/allYear/:quarter/allDuty/allCommand/', async function (
+  req,
+  res,
+  next
+) {
+  try {
+    console.log(
+      'api/graphData/incidentsBasisEncounter/allYear/:quarter/allDuty/allCommand/ route'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//7  a      :      a       :  <--All Years, Specific Quarter, All Injuries, All Duty, Specific Command
+router.get('/allYear/:quarter/allDuty/:command', async function (
+  req,
+  res,
+  next
+) {
+  try {
+    console.log(
+      'api/graphData/incidentsBasisEncounter/allYear/:quarter/allDuty/:command route'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//8  a      :      :       a  <--All Years, Specific Quarter, All Injuries, Specific Duty, All Command
+router.get('allYear/:quarter/:duty/allCommand', async function (
+  req,
+  res,
+  next
+) {
+  try {
+    console.log(
+      'api/graphData/incidentsBasisEncounter/allYear/:quarter/:duty/allCommand route'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//12 a      :      :       :  <--All Years, Specific Quarters, All Injuries, Specific Duty, Specific Command
+router.get('/allYear/:quarter/:duty/:command', async function (req, res, next) {
+  try {
+    console.log(
+      'api/graphData/incidentsBasisEncounter/allYear/:quarter/:duty/:command'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//5  :      a      a       a  <--Specific Year, All Quarters, All Injuries, All Duty, All Command
+router.get('/:year/allQuarter/allDuty/allCommand', async function (
+  req,
+  res,
+  next
+) {
+  try {
+    console.log(
+      'api/graphData/incidentsBasisEncounter/:year/allQuarter/allDuty/allCommand route'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//10 :      a      a       :  <--Specific Year, All Quarters, All Injuries, All Duty, Specific Command
+router.get('/:year/allQuarter/allDuty/:command', async function (
+  req,
+  res,
+  next
+) {
+  try {
+    console.log(
+      'api/graphData/incidentsBasisEncounter/:year/allQuarter/allDuty/:command'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//9  :      a      :       a  <--Specific Year, All Quarters, All Injuries, Specific Duty, All Command
+router.get('/:year/allQuarter/:duty/allCommand', async function (
+  req,
+  res,
+  next
+) {
+  try {
+    console.log(
+      'api/graphData/incidentsBasisEncounter/:year/allQuarter/:duty/allCommand'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//13 :      a      :       :  <--Specific Year, All Quarters, All Injuries, Specific Duty, Specific Command
+router.get('/:year/allQuarter/:duty/:command', async function (req, res, next) {
+  try {
+    console.log(
+      'api/graphData/incidentsBasisEncounter/:year/allQuarter/:duty/:command'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//11 :      :      a       a  <--Specific Year, Specific Quarter, All Injuries, All Duty, All Command
+router.get('/:year/:quarter/allDuty/allCommand', async function (
+  req,
+  res,
+  next
+) {
+  try {
+    console.log(
+      'api/graphData/incidentsBasisEncounter/:year/:quarter/allDuty/allCommand'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//14 :      :      a       :  <--Specific Year, Specific Quarters, All Injuries, All Duty, Specific Command
+router.get('/:year/:quarter/allDuty/:command', async function (req, res, next) {
+  try {
+    console.log(
+      'api/graphData/incidentsBasisEncounter/:year/:quarter/allDuty/:command'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//15 :      :      :       a  <--Specific Year, Specific Quarters, All Injuries, Specific Duty, All Command
+router.get('/:year/:quarter/:duty/allCommand', async function (req, res, next) {
+  try {
+    console.log(
+      'api/graphData/incidentsBasisEncounter/:year/:quarter/:duty/allCommand'
+    )
+  } catch (error) {
+    next(error)
+  }
+})
+
+//16 :      :      :       :  <--Specific Year, Specific Quarters, All Injuries, Specific Duty, Specific Command
+router.get('/:year/:quarter/:duty/:command', async function (req, res, next) {
+  try {
+    console.log(
+      'api/graphData/incidentsBasisEncounter/:year/:quarter/:duty/:command'
+    )
   } catch (error) {
     next(error)
   }
