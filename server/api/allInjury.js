8639c655234036bbca70a4d1328e6c902c672a6c
Kate Norton <katernorton@outlook.com>
2020-07-12 13:18:21 -0400

    api/graphData/allInjury/:year/:quarter/:duty/:command working

--- server/api/allInjury.js
+++ server/api/allInjury.js
@@ -707,6 +707,47 @@ router.get('/:year/:quarter/allDuty/:command', async function (req, res, next) {
 //15 :      :      :       a  <--Specific Year, Specific Quarters, All Injuries, Specific Duty, All Command
 router.get('/:year/:quarter/:duty/allCommand', async function (req, res, next) {
   try {
+    let timeFrame = await TimeFrame.findAll({
+      where: {year: req.params.year, quarter: req.params.quarter},
+    })
+    let duty
+    if (req.params.duty === 'on') duty = 'onDuty'
+    if (req.params.duty === 'off') duty = 'offDuty'
+    let timeFrameIds = timeFrame.map((item) => item.id)
+    let officerInjuriesData = []
+    for (let i = 0; i < timeFrameIds.length; i++) {
+      officerInjuriesData = [
+        ...officerInjuriesData,
+        ...(await OfficerInjury.findAll({
+          where: {timeFrameId: timeFrameIds[i]},
+          attributes: [duty],
+          include: [
+            {model: Command, attributes: ['commandName']},
+            {model: InjuryType, attributes: ['type']},
+            {model: TimeFrame, attributes: ['year', 'quarter']},
+          ],
+        })),
+      ]
+    }
+    let subjectInjuriesData = []
+    for (let i = 0; i < timeFrameIds.length; i++) {
+      subjectInjuriesData = [
+        ...subjectInjuriesData,
+        ...(await SubjectInjury.findAll({
+          where: {timeFrameId: timeFrameIds[i]},
+          attributes: [duty],
+          include: [
+            {model: Command, attributes: ['commandName']},
+            {model: InjuryType, attributes: ['type']},
+            {model: TimeFrame, attributes: ['year', 'quarter']},
+          ],
+        })),
+      ]
+    }
+    res.json({
+      officerData: officerInjuriesData,
+      subjectData: subjectInjuriesData,
+    })
     console.log('api/graphData/allInjury/:year/:quarter/:duty/allCommand')
   } catch (error) {
     next(error)
@@ -716,6 +757,50 @@ router.get('/:year/:quarter/:duty/allCommand', async function (req, res, next) {
 //16 :      :      :       :  <--Specific Year, Specific Quarters, All Injuries, Specific Duty, Specific Command
 router.get('/:year/:quarter/:duty/:command', async function (req, res, next) {
   try {
+    let timeFrame = await TimeFrame.findAll({
+      where: {year: req.params.year, quarter: req.params.quarter},
+    })
+    let command = await Command.findOne({
+      where: {commandName: req.params.command},
+    })
+    let duty
+    if (req.params.duty === 'on') duty = 'onDuty'
+    if (req.params.duty === 'off') duty = 'offDuty'
+    let timeFrameIds = timeFrame.map((item) => item.id)
+    let officerInjuriesData = []
+    for (let i = 0; i < timeFrameIds.length; i++) {
+      officerInjuriesData = [
+        ...officerInjuriesData,
+        ...(await OfficerInjury.findAll({
+          where: {timeFrameId: timeFrameIds[i], commandId: command.id},
+          attributes: [duty],
+          include: [
+            {model: Command, attributes: ['commandName']},
+            {model: InjuryType, attributes: ['type']},
+            {model: TimeFrame, attributes: ['year', 'quarter']},
+          ],
+        })),
+      ]
+    }
+    let subjectInjuriesData = []
+    for (let i = 0; i < timeFrameIds.length; i++) {
+      subjectInjuriesData = [
+        ...subjectInjuriesData,
+        ...(await SubjectInjury.findAll({
+          where: {timeFrameId: timeFrameIds[i], commandId: command.id},
+          attributes: [duty],
+          include: [
+            {model: Command, attributes: ['commandName']},
+            {model: InjuryType, attributes: ['type']},
+            {model: TimeFrame, attributes: ['year', 'quarter']},
+          ],
+        })),
+      ]
+    }
+    res.json({
+      officerData: officerInjuriesData,
+      subjectData: subjectInjuriesData,
+    })
     console.log('api/graphData/allInjury/:year/:quarter/:duty/:command')
   } catch (error) {
     next(error)
