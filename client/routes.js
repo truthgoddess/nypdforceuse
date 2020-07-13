d10359c29cf9293fd1849a79879abcbc5a4b6fc6
Kate Norton <katernorton@outlook.com>
2020-07-13 12:48:10 -0400

    /login, /data, /upload, / setup

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,14 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, DisplayContainer, Intro} from './components'
+import {
+  Login,
+  Signup,
+  UserHome,
+  DisplayContainer,
+  Intro,
+  Navbar,
+} from './components'
 import {me} from './store'
 
 /**
@@ -25,7 +32,7 @@ class Routes extends Component {
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
-            <Route path="/upload" component={UserHome} />
+            <Route path="/upload" component={Navbar} />
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
