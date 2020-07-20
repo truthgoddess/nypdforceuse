afb02ac82f26426560e07a7fd8dd00c8fb49fc6e
Kate Norton <katernorton@outlook.com>
2020-07-20 17:35:54 -0400

    upload duplicate problem fixed and uploads working

--- server/utility/batchCreate.js
+++ server/utility/batchCreate.js
@@ -11,89 +11,86 @@ const {
 } = require('../db/models')
 
 async function batchCreateSubjectInjury(subjectInjuryData) {
-  await Promise.all(
-    subjectInjuryData.map(async (item) => {
-      let [timeFrame] = await TimeFrame.findOrCreate({
-        where: {year: item.year, quarter: item.quarter},
-      })
-      let [command] = await Command.findOrCreate({
-        where: {commandName: item.command},
-      })
-      let [injuryType] = await InjuryType.findOrCreate({
-        where: {type: item.subjectInjury},
-      })
-      await SubjectInjury.create({
-        onDuty: item.onDuty,
-        offDuty: item.offDuty,
-        injuryTypeId: injuryType.id,
-        commandId: command.id,
-        timeFrameId: timeFrame.id,
-      })
+  for (let i = 0; i < subjectInjuryData.length; i++) {
+    let item = subjectInjuryData[i]
+    let [timeFrame] = await TimeFrame.findOrCreate({
+      where: {year: item.year, quarter: item.quarter},
     })
-  )
+    let [command] = await Command.findOrCreate({
+      where: {commandName: item.command},
+    })
+    let [injuryType] = await InjuryType.findOrCreate({
+      where: {type: item.subjectInjury},
+    })
+    await SubjectInjury.create({
+      onDuty: item.onDuty,
+      offDuty: item.offDuty,
+      injuryTypeId: injuryType.id,
+      commandId: command.id,
+      timeFrameId: timeFrame.id,
+    })
+  }
 }
 
 async function batchCreateOfficerInjury(officerInjuryData) {
-  await Promise.all(
-    officerInjuryData.map(async (item) => {
-      let [timeFrame] = await TimeFrame.findOrCreate({
-        where: {year: item.year, quarter: item.quarter},
-      })
-      let [command] = await Command.findOrCreate({
-        where: {commandName: item.command},
-      })
-      let [injuryType] = await InjuryType.findOrCreate({
-        where: {type: item.officerInjury},
-      })
-      await OfficerInjury.create({
-        onDuty: item.onDuty,
-        offDuty: item.offDuty,
-        injuryTypeId: injuryType.id,
-        commandId: command.id,
-        timeFrameId: timeFrame.id,
-      })
+  for (let i = 0; i < officerInjuryData.length; i++) {
+    let item = officerInjuryData[i]
+    let [timeFrame] = await TimeFrame.findOrCreate({
+      where: {year: item.year, quarter: item.quarter},
     })
-  )
+    let [command] = await Command.findOrCreate({
+      where: {commandName: item.command},
+    })
+    let [injuryType] = await InjuryType.findOrCreate({
+      where: {type: item.officerInjury},
+    })
+    await OfficerInjury.create({
+      onDuty: item.onDuty,
+      offDuty: item.offDuty,
+      injuryTypeId: injuryType.id,
+      commandId: command.id,
+      timeFrameId: timeFrame.id,
+    })
+  }
 }
+
 async function batchCreateIncidentsForceType(incidentsForceTypeData) {
-  await Promise.all(
-    incidentsForceTypeData.map(async (item) => {
-      let [timeFrame] = await TimeFrame.findOrCreate({
-        where: {year: item.year, quarter: item.quarter},
-      })
-      let [command] = await Command.findOrCreate({
-        where: {commandName: item.command},
-      })
-      let [forceCategory] = await ForceCategory.findOrCreate({
-        where: {type: item.forceCategory},
-      })
-      await IncidentsForceType.create({
-        onDuty: item.onDuty,
-        offDuty: item.offDuty,
-        forceCategoryId: forceCategory.id,
-        commandId: command.id,
-        timeFrameId: timeFrame.id,
-      })
+  for (let i = 0; i < incidentsForceTypeData.length; i++) {
+    let item = incidentsForceTypeData[i]
+    let [timeFrame] = await TimeFrame.findOrCreate({
+      where: {year: item.year, quarter: item.quarter},
     })
-  )
+    let [command] = await Command.findOrCreate({
+      where: {commandName: item.command},
+    })
+    let [forceCategory] = await ForceCategory.findOrCreate({
+      where: {type: item.forceCategory},
+    })
+    await IncidentsForceType.create({
+      onDuty: item.onDuty,
+      offDuty: item.offDuty,
+      forceCategoryId: forceCategory.id,
+      commandId: command.id,
+      timeFrameId: timeFrame.id,
+    })
+  }
 }
 
 async function batchCreateIncidentsBasisEncounter(incidentsBasisEncounterData) {
-  await Promise.all(
-    incidentsBasisEncounterData.map(async (item) => {
-      let [timeFrame] = await TimeFrame.findOrCreate({
-        where: {year: item.year, quarter: item.quarter},
-      })
-      let [encounterCategory] = await EncounterCategory.findOrCreate({
-        where: {type: item.basisForEncounter},
-      })
-      await IncidentsBasisEncounter.create({
-        count: item.count,
-        encounterCategoryId: encounterCategory.id,
-        timeFrameId: timeFrame.id,
-      })
+  for (let i = 0; i < incidentsBasisEncounterData.length; i++) {
+    let item = incidentsBasisEncounterData[i]
+    let [timeFrame] = await TimeFrame.findOrCreate({
+      where: {year: item.year, quarter: item.quarter},
     })
-  )
+    let [encounterCategory] = await EncounterCategory.findOrCreate({
+      where: {type: item.basisForEncounter},
+    })
+    await IncidentsBasisEncounter.create({
+      count: item.count,
+      encounterCategoryId: encounterCategory.id,
+      timeFrameId: timeFrame.id,
+    })
+  }
 }
 
 module.exports = {
