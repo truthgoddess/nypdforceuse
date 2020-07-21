894ee3e167a6178c3dfd4b36d3c42d34c0b42d29
Kate Norton <katernorton@outlook.com>
2020-07-20 20:33:52 -0400

    users removed

--- script/seed.js
+++ script/seed.js
@@ -7,14 +7,6 @@ const db = require('../server/db')
 async function seed() {
   await db.sync({force: true})
 
-  await User.create({
-    firstName: 'Kate',
-    lastName: 'Norton',
-    email: 'katernorton@outlook.com',
-    isAdmin: true,
-    password: '1g2Ik*iZJ40!',
-  })
-
   console.log('db synced!')
 }
 
