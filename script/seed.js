807a5fbca911077e6acd12cb0766de436ee3be0a
Kate Norton <katernorton@outlook.com>
2020-07-25 13:15:19 -0400

    protected admin user

--- script/seed.js
+++ script/seed.js
@@ -1,38 +1,38 @@
 'use strict'
 
-const {User} = require('../server/db/models')
-
 const db = require('../server/db')
 
-async function seed() {
-  await db.sync({force: true})
+// Seed code commented out so that accidental seeding does not occur, but left so that during development if needed can be used.
+// Eventually we'll get rid of this file entirely.
 
-  console.log('db synced!')
-}
+// async function seed() {
+//   await db.sync({force: true})
+//   console.log('db synced!')
+// }
 
-// We've separated the `seed` function from the `runSeed` function.
-// This way we can isolate the error handling and exit trapping.
-// The `seed` function is concerned only with modifying the database.
-async function runSeed() {
-  console.log('seeding...')
-  try {
-    await seed()
-  } catch (err) {
-    console.error(err)
-    process.exitCode = 1
-  } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
-  }
-}
+// // We've separated the `seed` function from the `runSeed` function.
+// // This way we can isolate the error handling and exit trapping.
+// // The `seed` function is concerned only with modifying the database.
+// async function runSeed() {
+//   console.log('seeding...')
+//   try {
+//     await seed()
+//   } catch (err) {
+//     console.error(err)
+//     process.exitCode = 1
+//   } finally {
+//     console.log('closing db connection')
+//     await db.close()
+//     console.log('db connection closed')
+//   }
+// }
 
-// Execute the `seed` function, IF we ran this module directly (`node seed`).
-// `Async` functions always return a promise, so we can use `catch` to handle
-// any errors that might occur inside of `seed`.
-if (module === require.main) {
-  runSeed()
-}
+// // Execute the `seed` function, IF we ran this module directly (`node seed`).
+// // `Async` functions always return a promise, so we can use `catch` to handle
+// // any errors that might occur inside of `seed`.
+// if (module === require.main) {
+//   runSeed()
+// }
 
-// we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+// // we export the seed function for testing purposes (see `./seed.spec.js`)
+// module.exports = seed
